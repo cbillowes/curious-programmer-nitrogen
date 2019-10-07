bf14c9b2d2aa9c04e3385f71cdfaa4525e742f26
Clarice Bouwer <cbillowes@gmail.com>
2019-10-07 03:22:19 +0200

    Remove unused Constants from logo.test.js

--- src/components/__tests__/logo.test.js
+++ src/components/__tests__/logo.test.js
@@ -2,7 +2,6 @@ import React from 'react'
 import Enzyme, { shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
-import Constants from './const'
 import Logo from '../logo'
 
 Enzyme.configure({ adapter: new Adapter() })
