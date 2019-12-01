f21bb79a118881d95b01deb2df4e705529ca739a
Clarice Bouwer <cbillowes@gmail.com>
2019-12-01 05:01:27 +0200

    Refactor navigation tests

--- src/components/__tests__/navigation.test.js
+++ src/components/__tests__/navigation.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import Enzyme, { shallow } from 'enzyme'
+import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
+import { containsValue } from './helpers'
 import Constants from './const'
 import Navigation from '../navigation'
 import Data from '../../../gatsby-data'
@@ -11,30 +12,34 @@ Enzyme.configure({ adapter: new Adapter() })
 
 describe('Navigation', () => {
   it('should open the navigation bar', () => {
-    const wrapper = shallow(<Navigation isOpen={true} />)
-    const rendered = wrapper.html()
-    const expected = `<nav class="open `
-    expect(rendered.indexOf(expected)).toBeGreaterThan(-1)
+    const contains = containsValue(
+      <Navigation isOpen={true} />,
+      `<nav class="open `,
+    )
+    expect(contains).toBe(true)
   })
 
   it('should close the navigation bar', () => {
-    const wrapper = shallow(<Navigation isOpen={false} />)
-    const rendered = wrapper.html()
-    const expected = `<nav class="closed `
-    expect(rendered.indexOf(expected)).toBeGreaterThan(-1)
+    const contains = containsValue(
+      <Navigation isOpen={false} />,
+      `<nav class="closed `,
+    )
+    expect(contains).toBe(true)
   })
 
   it('should set active item', () => {
-    const wrapper = shallow(<Navigation path='/about' />)
-    const rendered = wrapper.html()
-    const expected = `<span class="${Constants.SELECTED_MENU_ITEM_CLASS}"><a href="/about">About</a></span>`
-    expect(rendered.indexOf(expected)).toBeGreaterThan(-1)
+    const contains = containsValue(
+      <Navigation path="/about" />,
+      `<span class="${Constants.SELECTED_MENU_ITEM_CLASS}"><a href="/about">About</a></span>`, 
+    )
+    expect(contains).toBe(true)
   })
 
   it('should activate /blog for /', () => {
-    const wrapper = shallow(<Navigation path='/' />)
-    const rendered = wrapper.html()
-    const expected = `<span class="${Constants.SELECTED_MENU_ITEM_CLASS}"><a href="/blog">Blog</a></span>`
-    expect(rendered.indexOf(expected)).toBeGreaterThan(-1)
+    const contains = containsValue(
+      <Navigation path="/" />,
+      `<span class="${Constants.SELECTED_MENU_ITEM_CLASS}"><a href="/blog">Blog</a></span>`, 
+    )
+    expect(contains).toBe(true)
   })
 })
