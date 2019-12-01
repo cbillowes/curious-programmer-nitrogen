b1d9b2af1a7282945f74389f43ee857e28fb7b4c
Clarice Bouwer <cbillowes@gmail.com>
2019-12-01 04:29:12 +0200

    Add containsElement function to helpers and use in header tests

--- src/components/__tests__/header.test.js
+++ src/components/__tests__/header.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import Enzyme, { shallow } from 'enzyme'
+import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
+import { containsElement } from './helpers'
 import Header from '../header'
 import Logo from '../logo'
 import Search from '../search'
@@ -11,34 +12,34 @@ Enzyme.configure({ adapter: new Adapter() })
 
 describe('Header', () => {
   it('should render the logo', () => {
-    const header = shallow(<Header />)
-    const logo = shallow(<Logo />)
-    const renderedHeader = header.html()
-    const renderedLogo = logo.html()
-    expect(renderedHeader.indexOf(renderedLogo)).toBeGreaterThan(-1)
+    const contains = containsElement(
+      <Header />,
+      <Logo />,
+    )
+    expect(contains).toBe(true)
   })
 
   it('should render the search button', () => {
-    const header = shallow(<Header />)
-    const search = shallow(<Search />)
-    const renderedHeader = header.html()
-    const renderedSearch = search.html()
-    expect(renderedHeader.indexOf(renderedSearch)).toBeGreaterThan(-1)
+    const contains = containsElement(
+      <Header />,
+      <Search />,
+    )
+    expect(contains).toBe(true)
   })
 
   it('should render the menu button', () => {
-    const header = shallow(<Header />)
-    const menu = shallow(<Menu />)
-    const renderedHeader = header.html()
-    const renderedMenu = menu.html()
-    expect(renderedHeader.indexOf(renderedMenu)).toBeGreaterThan(-1)
+    const contains = containsElement(
+      <Header />,
+      <Menu />,
+    )
+    expect(contains).toBe(true)
   })
 
   it('should render the navigation', () => {
-    const header = shallow(<Header />)
-    const navigation = shallow(<Navigation />)
-    const renderedHeader = header.html()
-    const renderedNavigation = navigation.html()
-    expect(renderedHeader.indexOf(renderedNavigation)).toBeGreaterThan(-1)
+    const contains = containsElement(
+      <Header />,
+      <Navigation />,
+    )
+    expect(contains).toBe(true)
   })
 })
--- src/components/__tests__/helpers.js
+++ src/components/__tests__/helpers.js
@@ -1,8 +1,16 @@
-import { shallow } from 'enzyme'
+import { shallow } from "enzyme"
 
 export function containsValue(element, value) {
-    const wrapper = shallow(element)
-    const rendered = wrapper.html()
-    const contains = rendered.indexOf(value)
-    return contains > -1
+  const wrapper = shallow(element)
+  const rendered = wrapper.html()
+  const contains = rendered.indexOf(value)
+  return contains > -1
+}
+
+export function containsElement(parentElement, childElement) {
+  const parentWrapper = shallow(parentElement)
+  const childWrapper = shallow(childElement)
+  const renderedParent = parentWrapper.html()
+  const renderedChild = childWrapper.html()
+  return renderedParent.indexOf(renderedChild) > -1
 }
