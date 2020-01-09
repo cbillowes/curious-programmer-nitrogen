d7be27b8f3722ebe53339ca5325d00e7bf32e320
Clarice Bouwer <cbillowes@gmail.com>
2020-01-09 06:10:41 +0400

    Redefine credit stories

--- stories/credit.stories.js
+++ stories/credit.stories.js
@@ -4,5 +4,5 @@ import Credit from '../src/components/credit'
 import '../src/components/layout.css'
 
 storiesOf('Credit', module)
-  .add('with resource', () => <Credit to="https://www.google.com" title="Google">A super fast and popular search engine.</Credit>)
-  .add('without resource', () => <Credit title="Google">A super fast and popular search engine.</Credit>)
+  .add('simple', () => <Credit title="Important">This is a super duper important credit but does not link to any resource.</Credit>)
+  .add('has resource', () => <Credit to="https://www.google.com" title="Google">A super fast and popular search engine.</Credit>)
