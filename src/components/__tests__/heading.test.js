528ac0e8b3f003b6f8d849dafe321332d7f4a1ad
Clarice Bouwer <cbillowes@gmail.com>
2020-01-09 05:08:41 +0400

    Rather use containsElement instead of componentTranslatesTo in heading tests

--- src/components/__tests__/heading.test.js
+++ src/components/__tests__/heading.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
-import { componentTranslatesTo } from './helpers'
+import { containsElement } from './helpers'
 import { H1, H2, H3, H4 } from '../heading'
 
 Enzyme.configure({ adapter: new Adapter() })
@@ -9,35 +9,51 @@ Enzyme.configure({ adapter: new Adapter() })
 describe('Heading', () => {
 
   it('should render level one', () => {
-    const hasTranslated = componentTranslatesTo(
-      <H1>This is a h1</H1>,
-      <h1>This is a h1</h1>
+    const contains = containsElement(
+      <H1>
+        I am a big bold heading
+      </H1>,
+      <h1>
+        I am a big bold heading
+      </h1>,
     )
-    expect(hasTranslated).toBe(true)
+    expect(contains).toBe(true)
   })
 
   it('should render level two', () => {
-    const hasTranslated = componentTranslatesTo(
-      <H2>This is a h2</H2>,
-      <h2>This is a h2</h2>
+    const contains = containsElement(
+      <H2>
+        I am a big bold heading
+      </H2>,
+      <h2>
+        I am a big bold heading
+      </h2>,
     )
-    expect(hasTranslated).toBe(true)
+    expect(contains).toBe(true)
   })
 
   it('should render level three', () => {
-    const hasTranslated = componentTranslatesTo(
-      <H3>This is a h3</H3>,
-      <h3>This is a h3</h3>
+    const contains = containsElement(
+      <H3>
+        I am a big bold heading
+      </H3>,
+      <h3>
+        I am a big bold heading
+      </h3>,
     )
-    expect(hasTranslated).toBe(true)
+    expect(contains).toBe(true)
   })
 
   it('should render level four', () => {
-    const hasTranslated = componentTranslatesTo(
-      <H4>This is a h4</H4>,
-      <h4>This is a h4</h4>
+    const contains = containsElement(
+      <H4>
+        I am a big bold heading
+      </H4>,
+      <h4>
+        I am a big bold heading
+      </h4>,
     )
-    expect(hasTranslated).toBe(true)
+    expect(contains).toBe(true)
   })
 
 })
