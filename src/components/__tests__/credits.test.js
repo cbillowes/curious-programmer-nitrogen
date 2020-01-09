18e042a293f6ff52dce56bcae6b6fa51360f63dc
Clarice Bouwer <cbillowes@gmail.com>
2020-01-09 05:03:46 +0400

    Add simple unlinked tag to credit tests

--- src/components/__tests__/credits.test.js
+++ src/components/__tests__/credits.test.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import Enzyme, { shallow } from 'enzyme'
+import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import { containsElement } from './helpers'
 import Credit from '../credit'
-import { External } from "../tag"
+import { Bare, External } from "../tag"
 
 Enzyme.configure({ adapter: new Adapter() })
 
@@ -20,27 +20,37 @@ function sut(props) {
 
 describe('Credit', () => {
 
+  it('should render a simple tag for a resource that is not linked', () => {
+    const contains = containsElement(
+      sut({
+        title: `I don't link to anywhere`,
+        children: `Hello world`,
+      }),
+      <div>
+        <Bare>
+          I don't link to anywhere
+        </Bare>
+        Hello world
+      </div>,
+    )
+    expect(contains).toBe(true)
+  })
+
   it('should render a clickable tag to an external resource', () => {
     const contains = containsElement(
       sut({
         to: `https://www.google.com/`,
         title: `Google`,
+        children: `Take me to the Googles`
       }),
-      <External 
-        to="https://www.google.com/"
-      >
-        Google
-      </External>,
-    )
-    expect(contains).toBe(true)
-  })
-
-  it('should display content for that credit', () => {
-    const contains = containsElement(
-      sut({
-        children: <div>Hello World</div>,
-      }),
-      <div>Hello World</div>
+      <div>
+        <External 
+          to="https://www.google.com/"
+        >
+          Google
+        </External>
+        Take me to the Googles
+      </div>,
     )
     expect(contains).toBe(true)
   })
