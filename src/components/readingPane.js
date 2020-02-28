f16b9df0e29f83a5c64be3f397328437dcec713f
Clarice Bouwer <cbillowes@gmail.com>
2020-02-29 03:11:07 +0400

    Style the reading pane component

--- src/components/readingPane.js
+++ src/components/readingPane.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { H1 } from './heading'
 import { SmallSpacer } from './spacer'
+import './styles/ReadingPane.scss'
 
 const ReadingPane = ({ heading, children }) => {
   return (
     <div>
       <SmallSpacer />
       <H1>{heading}</H1>
-      <div className="reading-panel">
+      <div className="reading-pane">
         {children}
       </div>
     </div>
