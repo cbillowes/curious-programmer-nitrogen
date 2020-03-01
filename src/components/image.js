d2e2cd366df0ea7b0e87bdf5a2af35bbf06bb35b
Clarice Bouwer <cbillowes@gmail.com>
2020-03-02 01:15:51 +0400

    Refactor image component

--- src/components/image.js
+++ src/components/image.js
@@ -2,36 +2,50 @@ import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
 
-function render(file) {
-  if (file)
-    return <Img fluid={file.node.childImageSharp.fluid} />
-  return <></>
+function getFile (files, src) {
+  return files.images.edges.find(image =>
+    image.node.relativePath === src
+  )
 }
 
-function getImage(files, src) {
-  return files.images.edges.find(image => image.node.relativePath === src)
+function getFluidImage (file) {
+  return file.node.childImageSharp.fluid
 }
 
-const Image = ({ src }) => {
-  const images = useStaticQuery(graphql`
-    query {
-      images: allFile(filter: { extension: { regex: "/jpeg|jpg|png|gif/"}}) {
-        edges {
-          node {
-            extension
-            relativePath
-            childImageSharp {
-              fluid(maxWidth: 1980) {
-                ...GatsbyImageSharpFluid
-              }
+export function PureImage({ data, src }) {
+  const file = getFile(data, src)
+  if (!file) return <></>
+
+  const fluidImage = getFluidImage(file)
+  return (
+    <Img fluid={fluidImage} />
+  )
+}
+
+export const Image = props => {
+  const data = useStaticQuery(graphql`
+  query {
+    images: allFile(filter: { extension: { regex: "/jpeg|jpg|png|gif/"}}) {
+      edges {
+        node {
+          extension
+          relativePath
+          childImageSharp {
+            fluid(maxWidth: 1980) {
+              ...GatsbyImageSharpFluid
             }
           }
         }
       }
     }
+  }
   `)
-  const image = getImage(images, src)
-  return render(image)
+  return (
+    <PureImage
+      {...props}
+      data={data}
+    />
+  )
 }
 
 export default Image
