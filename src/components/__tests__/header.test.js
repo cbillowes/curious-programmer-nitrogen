f26264917e7285f21be624b71561935874ace424
Clarice Bouwer <cbillowes@gmail.com>
2020-03-02 10:54:36 +0200

    Revise header tests with snapshots

--- src/components/__tests__/header.test.js
+++ src/components/__tests__/header.test.js
@@ -1,111 +1,51 @@
 import React from 'react'
 import Header from '../header'
-import Logo from '../logo'
-import SearchBar from '../searchBar'
-import Navigation from '../navigation'
-import { containsElement, getMountedComponent } from './_helpers'
-
-function sut(props) {
-  return (
-    <Header
-      isMenuOpen={props.isMenuOpen}
-      isSearchOpen={props.isSearchOpen}
-    />
-  )
-}
-
-let header
+import { getMountedComponent, getSnapshot } from './_helpers'
 
 describe(`Header`, () => {
-
-  describe(`should contain the class name`, () => {
-
-    beforeEach(() => {
-      header = getMountedComponent(sut({}))
-    })
-
-    it(`for the component`, () => {
-      expect(header.find(`.header`).length).toBe(1)
-    })
-
-    it(`to align those components on the page`, () => {
-      expect(header.find(`.wrapper`).length).toBe(1)
-    })
-
+  it(`should render`, () => {
+    const tree = getSnapshot(
+      <Header />
+    )
+    expect(tree).toMatchSnapshot()
   })
 
-  describe(`should render the`, () => {
-
+  describe(`should hide`, () => {
+    let header
     beforeEach(() => {
-      header = sut({})
-    })
-
-    it(`logo`, () => {
-      const match = containsElement(
-        header,
-        <Logo />
-      )
-      expect(match).toBeTruthy()
-    })
-
-    it(`search bar`, () => {
-      const match = containsElement(
-        header,
-        <SearchBar />
-      )
-      expect(match).toBeTruthy()
+      header = getMountedComponent(
+        <Header
+          isMenuOpen={false}
+          isSearchOpen={false}
+        />)
     })
 
     it(`navigation`, () => {
-      const match = containsElement(
-        header,
-        <Navigation />
+      expect(header.find(`.navigation`).hasClass(`closed`)).toBe(true)
+    })
+
+    it(`search`, () => {
+      expect(header.find(`.search-button`).hasClass(`active`)).toBe(false)
+    })
+  })
+
+  describe(`should show`, () => {
+    let header
+    beforeEach(() => {
+      header = getMountedComponent(
+        <Header
+          isMenuOpen={true}
+          isSearchOpen={true}
+        />
       )
-      expect(match).toBeTruthy()
-    })
-
-  })
-
-  describe(`should hide elements when unactiviated, including the`, () => {
-
-    let component
-
-    beforeEach(() => {
-      component = getMountedComponent(sut({
-        isMenuOpen: false,
-        isSearchOpen:false,
-      }))
     })
 
     it(`navigation`, () => {
-      expect(component.find(`.navigation`).hasClass(`closed`)).toBe(true)
+      expect(header.find(`.navigation`).hasClass(`open`)).toBe(true)
     })
 
-    it(`search bar`, () => {
-      expect(component.find(`.search-button`).hasClass(`active`)).toBe(false)
+    it(`search`, () => {
+      expect(header.find(`.search-button`).hasClass(`active`)).toBe(true)
     })
-
   })
-
-  describe(`should show elements when activated, including the`, () => {
-
-    let component
-
-    beforeEach(() => {
-      component = getMountedComponent(sut({
-        isMenuOpen: true,
-        isSearchOpen: true,
-      }))
-    })
-
-    it(`navigation`, () => {
-      expect(component.find(`.navigation`).hasClass(`open`)).toBe(true)
-    })
-
-    it(`search bar`, () => {
-      expect(component.find(`.search-button`).hasClass(`active`)).toBe(true)
-    })
-
-  })
-
 })
