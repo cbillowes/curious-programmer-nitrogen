3a56c6a6d5c5c455f3a881e428902f94ed933aa6
Clarice Bouwer <cbillowes@gmail.com>
2020-03-02 15:58:32 +0200

    Revise heading tests with snapshots

--- src/components/__tests__/heading.test.js
+++ src/components/__tests__/heading.test.js
@@ -1,79 +1,44 @@
 import React from 'react'
-import { getMountedComponent } from "./_helpers"
+import { getSnapshot } from "./_helpers"
 import { H1, H2, H3, H4 } from "../heading"
 
 describe(`Heading`, () => {
-
-  describe(`should render`, () => {
-
-    it(`h1`, () => {
-      const heading = getMountedComponent(<H1 />)
-      expect(heading).toBeDefined()
-    })
-
-    it(`h2`, () => {
-      const heading = getMountedComponent(<H2 />)
-      expect(heading).toBeDefined()
-    })
-
-    it(`h3`, () => {
-      const heading = getMountedComponent(<H3 />)
-      expect(heading).toBeDefined()
-    })
-
-    it(`h4`, () => {
-      const heading = getMountedComponent(<H4 />)
-      expect(heading).toBeDefined()
-    })
-
+  it(`should not render when empty`, () => {
+    const empties = (
+      <div>
+        <H1 />
+        <H2 />
+        <H3 />
+        <H4 />
+      </div>
+    )
+    const tree = getSnapshot(empties)
+    expect(tree).toMatchSnapshot()
   })
 
-  describe(`should display heading for`, () => {
-
-    it(`h1`, () => {
-      const heading = getMountedComponent(<H1>Don't Panic.</H1>)
-      expect(heading.text()).toEqual(`Don't Panic.`)
-    })
-
-    it(`h2`, () => {
-      const heading = getMountedComponent(<H2>Time is an illusion. Lunchtime doubly so.</H2>)
-      expect(heading.text()).toEqual(`Time is an illusion. Lunchtime doubly so.`)
-    })
-
-    it(`h3`, () => {
-      const heading = getMountedComponent(<H3>So long and thanks for all the fish</H3>)
-      expect(heading.text()).toEqual(`So long and thanks for all the fish`)
-    })
-
-    it(`h4`, () => {
-      const heading = getMountedComponent(<H4>The ships hung in the sky in much the same way that bricks don't.</H4>)
-      expect(heading.text()).toEqual(`The ships hung in the sky in much the same way that bricks don't.`)
-    })
-
+  it(`should render`, () => {
+    const headings = (
+      <div>
+        <H1>Don't panic.</H1>
+        <H2>Time is an illusion. Lunchtime doubly so.</H2>
+        <H3>So long and thanks for all the fish.</H3>
+        <H4>The ships hung in the sky in much the same way that bricks don't.</H4>
+      </div>
+    )
+    const tree = getSnapshot(headings)
+    expect(tree).toMatchSnapshot()
   })
 
-  describe(`should set the class for`, () => {
-
-    it(`h1`, () => {
-      const heading = getMountedComponent(<H1 className="one" />)
-      expect(heading.hasClass(`one`)).toBe(true)
-    })
-
-    it(`h2`, () => {
-      const heading = getMountedComponent(<H2 className="two" />)
-      expect(heading.hasClass(`two`)).toBe(true)
-    })
-
-    it(`h3`, () => {
-      const heading = getMountedComponent(<H3 className="three" />)
-      expect(heading.hasClass(`three`)).toBe(true)
-    })
-
-    it(`h4`, () => {
-      const heading = getMountedComponent(<H4 className="four" />)
-      expect(heading.hasClass(`four`)).toBe(true)
-    })
-
+  it(`should capture class names`, () => {
+    const headings = (
+      <div>
+        <H1 className="un">un</H1>
+        <H2 className="deux">deux</H2>
+        <H3 className="trois">trois</H3>
+        <H4 className="quatre">quatre</H4>
+      </div>
+    )
+    const tree = getSnapshot(headings)
+    expect(tree).toMatchSnapshot()
   })
-
 })
--- src/components/heading.js
+++ src/components/heading.js
@@ -1,24 +1,32 @@
 import React from 'react'
 
 const H1 = ({ className, children }) => {
+  if (!children) return <></>
+
   return (
     <h1 className={className}>{children}</h1>
   )
 }
 
 const H2 = ({ className, children }) => {
+  if (!children) return <></>
+
   return (
     <h2 className={className}>{children}</h2>
   )
 }
 
 const H3 = ({ className, children }) => {
+  if (!children) return <></>
+
   return (
     <h3 className={className}>{children}</h3>
   )
 }
 
 const H4 = ({ className, children }) => {
+  if (!children) return <></>
+
   return (
     <h4 className={className}>{children}</h4>
   )
