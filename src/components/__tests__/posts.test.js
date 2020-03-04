3f8f939b5179066249f0f1e20daa8ac6fbe178e3
Clarice Bouwer <cbillowes@gmail.com>
2020-03-04 19:43:13 +0200

    Add defaultProps, propTypes and snapshots for Posts component

--- src/components/__tests__/posts.test.js
+++ src/components/__tests__/posts.test.js
@@ -3,9 +3,43 @@ import Posts from '../posts'
 import { getSnapshot } from './_helpers'
 
 describe(`Posts`, () => {
-  it(`should render`, () => {
+  const edges = [
+    {
+      node: {
+        excerpt: ``,
+        timeToRead: 1,
+        html: `,`,
+        fields: {
+          slug: `/dont-panic`,
+          date: new Date(""),
+          number: 42,
+        },
+        frontmatter: {
+          title: ``,
+          tags: [],
+          author: `Clarice Bouwer`,
+          blurb: ``,
+        }
+      }
+    }
+  ]
+
+  it(`should render with default word limit`, () => {
     const posts = (
-      <Posts />
+      <Posts
+        edges={edges}
+      />
+    )
+    const tree = getSnapshot(posts)
+    expect(tree).toMatchSnapshot()
+  })
+
+  it(`should render with specified word limit of 5`, () => {
+    const posts = (
+      <Posts
+        edges={edges}
+        limit={5}
+      />
     )
     const tree = getSnapshot(posts)
     expect(tree).toMatchSnapshot()
--- src/components/posts.js
+++ src/components/posts.js
@@ -1,11 +1,10 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import PostPreview from './postPreview'
 import Lang from '../../gatsby-lang'
 import './styles/posts.scss'
 
-const defaultTruncate = 50
-
-const Listing = ({ edges, truncate }) => {
+const Listing = ({ edges, limit }) => {
   return edges.map(edge => {
     const { timeToRead, excerpt } = edge.node
     const { slug, date, number } = edge.node.fields
@@ -14,7 +13,7 @@ const Listing = ({ edges, truncate }) => {
     return (
       <PostPreview
         key={slug}
-        limit={truncate ? truncate : defaultTruncate}
+        limit={limit}
         title={title}
         slug={slug}
         tags={tagCollection}
@@ -28,22 +27,30 @@ const Listing = ({ edges, truncate }) => {
   })
 }
 
-const Posts = ({ edges, truncate }) => {
+export const Posts = ({ edges, limit }) => {
   if (!edges || edges.length === 0) return <div>{Lang.noPostsAvailable}</div>
 
   return (
     <div className="posts">
       <Listing
         edges={edges}
-        truncate={truncate}
+        truncate={limit}
       />
     </div>
   )
 }
 
+Posts.defaultProps = {
+  limit: 50,
+}
+
+Posts.propTypes = {
+  edges: PropTypes.object.isRequired,
+  limit: PropTypes.number,
+}
+
 export default Posts
 
-//TODO: add proptypes
 //TODO: serious housekeeping required in this component
 //TODO: move Lang to gatsby-config?
 //TODO: somewhere the actual post needs to be rendered
