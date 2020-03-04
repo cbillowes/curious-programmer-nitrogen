79a803c192e1f01053c0eca097802d87da820c1a
Clarice Bouwer <cbillowes@gmail.com>
2020-03-04 10:57:13 +0200

    Test Spacer component

--- src/components/__tests__/spacer.test.js
+++ src/components/__tests__/spacer.test.js
@@ -1,13 +1,32 @@
 import React from 'react'
-import Spacer from '../spacer'
+import { Spacer, LargeSpacer, SmallSpacer } from '../spacer'
 import { getSnapshot } from './_helpers'
 
-describe.skip(`Spacer`, () => {
-  it(`should render`, () => {
+describe(`Spacer`, () => {
+
+  it(`should render default`, () => {
     const spacer = (
       <Spacer />
     )
     const tree = getSnapshot(spacer)
     expect(tree).toMatchSnapshot()
   })
+
+  it(`should render large`, () => {
+    const spacer = (
+      <LargeSpacer />
+    )
+    const tree = getSnapshot(spacer)
+    expect(tree).toMatchSnapshot()
+  })
+
+  it(`should render small`, () => {
+    const spacer = (
+      <SmallSpacer />
+    )
+    const tree = getSnapshot(spacer)
+    expect(tree).toMatchSnapshot()
+  })
 })
+
+//TODO: do I need all these spacers?
