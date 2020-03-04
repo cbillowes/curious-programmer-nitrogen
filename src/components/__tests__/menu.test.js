791a4be364e9a4fffbe1e02646a83b2bb169fb1b
Clarice Bouwer <cbillowes@gmail.com>
2020-03-04 20:53:24 +0200

    Add propTypes, defaultProps and update snapshot for Menu component

--- src/components/__tests__/menu.test.js
+++ src/components/__tests__/menu.test.js
@@ -5,7 +5,31 @@ import { getSnapshot } from './_helpers'
 describe(`Menu`, () => {
   it(`should render`, () => {
     const menu = (
-      <Menu />
+      <Menu
+        toggleOnClick={jest.fn()}
+      />
+    )
+    const tree = getSnapshot(menu)
+    expect(tree).toMatchSnapshot()
+  })
+
+  it(`should render opened`, () => {
+    const menu = (
+      <Menu
+        toggleOnClick={jest.fn()}
+        isOpen={true}
+      />
+    )
+    const tree = getSnapshot(menu)
+    expect(tree).toMatchSnapshot()
+  })
+
+  it(`should render closed`, () => {
+    const menu = (
+      <Menu
+        toggleOnClick={jest.fn()}
+        isOpen={false}
+      />
     )
     const tree = getSnapshot(menu)
     expect(tree).toMatchSnapshot()
--- src/components/menu.js
+++ src/components/menu.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEllipsisV } from '@fortawesome/free-solid-svg-icons'
 import './styles/menu.scss'
@@ -16,6 +17,13 @@ const Menu = ({ toggleOnClick, isOpen }) => {
   )
 }
 
-export default Menu
+Menu.defaultProps = {
+  isOpen: false,
+}
 
-//TODO: add proptypes
+Menu.propTypes = {
+  toggleOnClick: PropTypes.func.isRequired,
+  isOpen: PropTypes.bool,
+}
+
+export default Menu
