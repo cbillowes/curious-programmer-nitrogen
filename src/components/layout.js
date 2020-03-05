aecb0776b17ddd78309437925f6e24cfbfadddf3
Clarice Bouwer <cbillowes@gmail.com>
2020-03-05 14:29:42 +0200

    Add prismjs for gatsby pages

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import PropTypes from 'prop-types'
+import Prism from 'prismjs'
 import Header from './header'
 import Footer from './footer'
 import Constants from '../../gatsby-data'
@@ -8,6 +9,12 @@ import './styles/layout.scss'
 const site = Constants.site
 
 const Layout = ({ className, reading, footer, children }) => {
+  // The definitive guide for using PrismJs in Gatsby
+  // https://dev.to/fidelve/the-definitive-guide-for-using-prismjs-in-gatsby-4708
+  useEffect(() => {
+    Prism.highlightAll()
+  })
+
   return (
     <div className="page">
       <Header siteTitle={site.title} />
