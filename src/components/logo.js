3d3aedc0053e67f92494781af648799b40e48f21
Clarice Bouwer <cbillowes@gmail.com>
2020-03-08 02:39:38 +0200

    Create responsive logo

--- src/components/logo.js
+++ src/components/logo.js
@@ -2,20 +2,23 @@ import React from 'react'
 import Anchor from './anchor'
 import './styles/logo.scss'
 
-const Content = () => (
-  <>
-    <strong>&#123;</strong> :curious <strong>"programmer"</strong> <strong>&#125;</strong>
-  </>
-)
-
 function Logo() {
   return (
     <div className="logo">
       <Anchor
         to="/"
         title="Curious Programmer"
+        className="full"
       >
-        <Content />
+        <strong>&#123;</strong> :curious <strong>"programmer"</strong> <strong>&#125;</strong>
+      </Anchor>
+
+      <Anchor
+        to="/"
+        title="Curious Programmer"
+        className="partial"
+      >
+        <strong>&#123;</strong> :curious <strong>&#125;</strong>
       </Anchor>
     </div>
   )
