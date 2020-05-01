7e20384fb87905c8513d58e55b9555fc87212eff
Clarice Bouwer <cbillowes@gmail.com>
2020-05-01 13:10:04 +0400

    Revise anchor stories

--- stories/anchor.stories.js
+++ stories/anchor.stories.js
@@ -6,19 +6,17 @@ import Page from "./_helpers"
 storiesOf("Components/anchor", module)
   .add("internal", () => (
     <Page>
-      <Anchor to="/about" title="About Clarice Bouwer">
-        About
-      </Anchor>
+      <p>
+        <strong>Internal link in a sentence:</strong> I have been a software
+        developer for over 10 years. Find out more
+        <Anchor to="/about" title="About">
+          about
+        </Anchor>
+        me and feel free to get in touch.
+      </p>
     </Page>
   ))
   .add("external", () => (
-    <Page>
-      <Anchor to="https://curiousprogrammer.dev" title="Curious Programmer">
-        Curious Programmer
-      </Anchor>
-    </Page>
-  ))
-  .add("snippet", () => (
     <Page>
       <p>
         <strong>External links in a sentence:</strong> I share what I learn with
@@ -34,21 +32,25 @@ storiesOf("Components/anchor", module)
         <Anchor to="https://www.netlify.com/" title="Netlify">
           Netlify
         </Anchor>
-        .
-      </p>
-      <p>
-        <strong>Internal link in a sentence:</strong> I have been a software
-        developer for over 10 years. Find out more
-        <Anchor to="/about" title="About">
-          about
+        . This link has not been
+        <Anchor to="http://this.does.not.exist" title="Unvisitable">
+          visited
         </Anchor>
-        me and feel free to get in touch.
+        yet.
       </p>
+    </Page>
+  ))
+  .add("unvisited link", () =>
+    <Page>
       <p>
         <strong>Guaranteed unvisited link:</strong> To make 100% sure, here is a
         link I<Anchor to="https://unvisited.link">haven't</Anchor>
         visited yet.
       </p>
+    </Page>
+  )
+  .add("group", () => (
+    <Page>
       <p>
         <strong>Links all together now: </strong>
         <Anchor to="https://link.one" title="Gatsby">
