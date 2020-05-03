9cd6bd274956b3adebc8ae60d77e8c02a834b568
Clarice Bouwer <cbillowes@gmail.com>
2020-05-03 08:55:46 +0400

    Extract the indexing of posts for searching into seperate file

---
+++ gatsby-build/search.js
@@ -0,0 +1,110 @@
+const _ = require("lodash")
+const path = require("path")
+const fs = require("fs")
+const config = require("../gatsby-build")
+const createIndexes = true
+const demoPage = `/blog/example`
+
+// Where all the incremental search index files go to live
+const exportDirectory = path.join(__dirname, `/../export`)
+
+/**
+ *
+ * @param {*} graphql
+ * @return {Promise}
+ */
+const query = async (graphql) => {
+  return await graphql(`
+    query CreateSearchIndexesQuery {
+      allMarkdownRemark(sort: { order: ASC, fields: fields___date }) {
+        edges {
+          node {
+            html
+            fields {
+              slug
+            }
+            frontmatter {
+              title
+              tags
+            }
+          }
+        }
+      }
+    }
+  `)
+}
+
+const toTimestamp = (date) => {
+  return new Date(date).getTime()
+}
+
+const getHistoryOfPreviouslyExportedSlugs = () => {
+  const files = fs.readdirSync(exportDirectory).filter(file => file !== `slugs.txt`)
+  const slugs = files.map(file => {
+    const filePath = path.join(exportDirectory, file)
+    const data = fs.readFileSync(filePath, { encoding: `utf-8` })
+    const json = JSON.parse(data)
+    return json.map(j => {
+      return j.slug
+    })
+  })
+  return [].concat.apply([], slugs)
+}
+
+const tidyIndexableContent = (html) => {
+  return html
+    .replace(/<[^>]+>/gm, ``)
+    .replace(/([\r\n]+ +)+/gm, ``)
+    .replace(/\n/g, ` `)
+}
+
+const storeIncrementalExport = (posts) => {
+  if (posts.length === 0) return
+  const filename = `${exportDirectory}/search-${toTimestamp(new Date())}.json`
+  fs.writeFileSync(filename, JSON.stringify(posts), `utf-8`)
+}
+
+const storeIncrementalExportLog = (slugs) => {
+  if (slugs.length === 0) return
+  const date = new Date()
+  const data = slugs.reduce((result, slug) => {
+    return `${result}\n${date}: ${slug}`
+  }, "")
+  fs.appendFileSync(path.join(exportDirectory, `slugs.txt`), data, { encoding: `utf-8` })
+}
+
+module.exports.create = async (graphql, reporter) => {
+  if (!createIndexes) {
+    reporter.warn(`off: create search indexes`)
+    return
+  }
+
+  query(graphql)
+    .then(result => {
+      if (result.errors) {
+        reporter.error(`create search indexes: ${result.errors}`)
+      }
+
+      const history = getHistoryOfPreviouslyExportedSlugs()
+      const edges = result.data.allMarkdownRemark.edges
+        .filter(edge => edge.node.fields.slug !== demoPage)
+        .filter(edge => history.indexOf(edge.node.fields.slug) === -1)
+
+      const posts = edges.map(edge => {
+        const node = edge.node
+        const slug = node.fields.slug
+        if (history.indexOf(slug) > -1) return
+
+        reporter.verbose(`index: ${slug}`)
+        return {
+          slug,
+          title: node.frontmatter.title,
+          tags: node.frontmatter.tags,
+          html: tidyIndexableContent(node.html),
+        }
+      })
+
+      storeIncrementalExport(posts)
+      storeIncrementalExportLog(posts.map(post => post.slug))
+    })
+}
--- gatsby-node.js
+++ gatsby-node.js
@@ -4,6 +4,7 @@ const fs = require("fs")
 const images = require("./gatsby-build/images")
 const blog = require("./gatsby-build/pages-blog")
 const tags = require("./gatsby-build/pages-tags")
+const search = require("./gatsby-build/search")
 
 // The order of which nodes are processed is no guaranteed.
 // To add numbers to each post, nodes need to be captured
@@ -19,9 +20,6 @@ const nodes = []
 // It must however be available for display as its own entity
 // accessible only by its slug
 const exclusionSlugForPost = `/blog/example`
-
-// Where all the incremental search index files go to live
-const exportDirectory = path.join(`${__dirname}`, `export`)
 
 /**
  * GATSBY API.
@@ -48,7 +46,7 @@ exports.onCreateNode = ({ node, actions, reporter }) => {
 exports.createPages = async ({ graphql, actions, reporter }) => {
   await blog.create(actions, graphql, reporter)
   await tags.create(actions, graphql, reporter)
-  await createSearchIndexes(graphql, reporter)
+  await search.create(graphql, reporter)
 }
 
 // This is the part where numbers and any other graphql fields can be added.
@@ -90,54 +88,6 @@ const createNodes = (node, createNodeField) => {
   nodes.push(node)
 }
 
-const createSearchIndexes = async (graphql, reporter) => {
-  await graphql(`
-    query CreateSearchIndexesQuery {
-      allMarkdownRemark(sort: { order: ASC, fields: fields___date }) {
-        edges {
-          node {
-            html
-            fields {
-              slug
-            }
-            frontmatter {
-              title
-              tags
-            }
-          }
-        }
-      }
-    }
-  `)
-    .then(async result => {
-      if (result.errors) {
-        reporter.error(`something broke`)
-        return
-      }
-
-      // Let's be sure not to index the demo page.
-      const slugs = getPreviousExportedSlugs()
-      const edges = edgesWithoutDemoPost(result)
-        .filter(edge => slugs.indexOf(edge.node.fields.slug) === -1)
-
-      const posts = edges.map(edge => {
-        const node = edge.node
-        const slug = node.fields.slug
-        if (slugs.indexOf(slug) > -1) return
-
-        return {
-          slug,
-          title: node.frontmatter.title,
-          tags: node.frontmatter.tags,
-          html: tidyIndexableContent(node.html),
-        }
-      })
-
-      storeIncrementalExport(posts)
-      storeIncrementalExportLog(posts.map(post => post.slug))
-    })
-}
-
 const applyNumbers = (createNodeField) => {
   const numberable = nodes.filter(node => node.fields !== exclusionSlugForPost)
   const sorted = numberable.sort(
@@ -164,65 +114,4 @@ const toTimestamp = (date) => {
 const edgesWithoutDemoPost = (graphqlResult) => {
   return graphqlResult.data.allMarkdownRemark.edges
     .filter(edge => edge.node.fields.slug !== exclusionSlugForPost)
-}
-
-const getPreviousNode = (edges, index) => {
-  const i = index === 0 ? edges.length - 1 : index - 1
-  return {
-    number: i + 1,
-    node: edges[i].node,
-  }
-}
-
-const getNextNode = (edges, index) => {
-  const i = index === edges.length - 1 ? 0 : index + 1
-  return {
-    number: i + 1,
-    node: edges[i].node,
-  }
-}
-
-const getPost = (edges, index) => {
-  const node = edges[index].node
-  return {
-    number: index + 1,
-    slug: node.fields.slug,
-    date: node.fields.date,
-    previous: getPreviousNode(edges, index),
-    next: getNextNode(edges, index),
-  }
-}
-
-const tidyIndexableContent = (html) => {
-  return html
-    .replace(/<[^>]+>/gm, ``)
-    .replace(/([\r\n]+ +)+/gm, ``)
-    .replace(/\n/g, ` `)
-}
-
-const getPreviousExportedSlugs = () => {
-  const files = fs.readdirSync(exportDirectory).filter(file => file !== `slugs.txt`)
-  const slugs = files.map(file => {
-    const data = fs.readFileSync(path.join(exportDirectory, file), { encoding: `utf-8` })
-    const json = JSON.parse(data)
-    return json.map(j => {
-      return j.slug
-    })
-  })
-  return [].concat.apply([], slugs)
-}
-
-const storeIncrementalExport = (posts) => {
-  if (posts.length === 0) return
-  const filename = `${__dirname}/export/search-${toTimestamp(new Date())}.json`
-  fs.writeFileSync(filename, JSON.stringify(posts), `utf-8`)
-}
-
-const storeIncrementalExportLog = (slugs) => {
-  if (slugs.length === 0) return
-  const date = new Date()
-  const data = slugs.reduce((result, slug) => {
-    return `${result}\n${date}: ${slug}`
-  }, "")
-  fs.appendFileSync(path.join(exportDirectory, `slugs.txt`), data, { encoding: `utf-8` })
 }
